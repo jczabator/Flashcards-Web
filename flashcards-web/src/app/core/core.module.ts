c3416a94357363eaecb2f23216344250ab73032f
cjbg <cjbg@o2.pl>
2017-03-02 13:28:46 +0100

    Add navigation menu

--- flashcards-web/src/app/core/core.module.ts
+++ flashcards-web/src/app/core/core.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from '@angular/material';
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 import { NavigationComponent } from "./navigation/navigation.component";
 
 @NgModule({
     imports: [
         CommonModule,
-        MaterialModule
+        MaterialModule,
+        FlexLayoutModule
     ],
     exports:[
         CommonModule,
